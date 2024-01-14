8cec9741f125b36cb4bc32237a4ee6aa4f4cdfb2
Hiren chandro <hirenchandro5215@gmail.com>
2024-01-14 16:47:18 +0600

    Loging code correction

--- js/bank.js
+++ js/bank.js
@@ -1,13 +1,13 @@
-// document.getElementById('loging-button').addEventListener('click', function () {
-//     const emailField = document.getElementById('email-field');
-//     const userMail = emailField.value;
-//     const passwordField = document.getElementById('password-field');
-//     const userPassword = passwordField.value;
-//     console.log(userPassword);
-//     console.log(userMail);
-// if ((userMail == 'h') && (userPassword == '1')) {
-// document.body.style.backgroundColor='red';
-// window.location.href = 'bank.html';
-// console.log('hiren');
-// }
-// })
+document.getElementById('loging-button').addEventListener('click', function () {
+    const emailField = document.getElementById('email-field');
+    const userMail = emailField.value;
+    const passwordField = document.getElementById('password-field');
+    const userPassword = passwordField.value;
+    // console.log(userPassword);
+    // console.log(userMail);
+if ((userMail =='hiren@gmail.com') && (userPassword == '123')) {
+
+window.location.href = 'index.html';
+
+}
+})
